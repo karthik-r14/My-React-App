37107631c569656ee75dd49f2460ddd011148d91
karthikramachandran <kramachandran@equalexperts.com>
2019-02-19 21:35:13 +0530

    Now, JSX has two components named Header and Component.

--- src/App.js
+++ src/App.js
@@ -5,21 +5,39 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header />
+        <Content />
+      </div>
+    );
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>{1 + 1}</h1>
+        <h1>My first React app</h1>
+      </div>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <div>
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
+        <a
+          className="App-link"
+          href="https://www.google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open Google
+        </a>
       </div>
     );
   }
